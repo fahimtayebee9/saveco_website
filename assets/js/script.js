0ca7bfb73598deee7d06ddf635c5ffbfd75761a9
fahimtayebee9 <fahimtayebee9@gmail.com>
2021-05-23 14:52:05 +0600

    Full Complete WORK

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 function openNav() {
-    if (window.screen.width < 992) {
-        document.getElementById("mb_nav_body").style.width = "100%";
-        document.getElementById("mb_nav").style.width = "300px";
-    }
+  if (window.screen.width < 992) {
+    document.getElementById("mb_nav_body").style.width = "100%";
+    document.getElementById("mb_nav").style.width = "300px";
+  }
 }
 
 function closeNav() {
-    document.getElementById("mb_nav_body").style.width = "0%";
-    document.getElementById("mb_nav").style.width = "0";
+  document.getElementById("mb_nav_body").style.width = "0%";
+  document.getElementById("mb_nav").style.width = "0";
 }
 
